edf84699f5f658b9a1f0e334193d802ffae0471e
Bruce (Musician101) <bruceabivens@gmail.com>
2018-01-22 05:57:17 -0800

    Some code clean up. Also make sure kickPlayers() actually runs when /whitelist on or /whitelist reload are entered.

--- whitelistfix-sponge/src/main/java/io/musician101/whitelistfix/sponge/mixin/MixinCommandWhitelist.java
+++ whitelistfix-sponge/src/main/java/io/musician101/whitelistfix/sponge/mixin/MixinCommandWhitelist.java
@@ -4,7 +4,6 @@ import io.musician101.whitelistfix.Reference;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.server.CommandWhitelist;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.TextComponentString;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.service.whitelist.WhitelistService;
 import org.spongepowered.api.text.Text;
@@ -18,26 +17,17 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public abstract class MixinCommandWhitelist {
 
     private void kickPlayers() {
-        Sponge.getServiceManager().provide(WhitelistService.class).ifPresent(whitelistService -> {
-            Sponge.getServer().getOnlinePlayers().stream().filter(player -> whitelistService.isWhitelisted(player.getProfile())).forEach(player -> {
-                player.kick(Text.of(Reference.KICK_REASON));
-            });
-        });
-    }
-
-    @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;setWhiteListEnabled(Z)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
-    public void off(MinecraftServer server, ICommandSender sender, String[] args, CallbackInfo ci) {
-        sender.sendMessage(new TextComponentString(args[0]));
+        Sponge.getServiceManager().provide(WhitelistService.class).ifPresent(whitelistService -> Sponge.getServer().getOnlinePlayers().stream().filter(player -> whitelistService.isWhitelisted(player.getProfile())).forEach(player -> player.kick(Text.of(Reference.KICK_REASON))));
     }
 
     @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;setWhiteListEnabled(Z)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
     public void on(MinecraftServer server, ICommandSender sender, String[] args, CallbackInfo ci) {
-        sender.sendMessage(new TextComponentString(args[0]));
+        kickPlayers();
     }
 
     @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;reloadWhitelist()V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
     public void reload(MinecraftServer server, ICommandSender sender, String[] args, CallbackInfo ci) {
-        sender.sendMessage(new TextComponentString(args[0]));
+        kickPlayers();
     }
 
     @Inject(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;removePlayerFromWhitelist(Lcom/mojang/authlib/GameProfile;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
